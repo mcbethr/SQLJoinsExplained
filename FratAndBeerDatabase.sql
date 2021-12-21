6382a6f63ffd81cdca7a3e2d0f4208123fc2cc61
mcbethr <51751681+mcbethr@users.noreply.github.com>
2021-12-21 06:06:45 -0500

    Update FratAndBeerDatabase.sql
    Added select statements

--- FratAndBeerDatabase.sql
+++ FratAndBeerDatabase.sql
@@ -27,3 +27,39 @@ insert into Beers (FratID,BeerName) Values (Null,'Natty Lite 9')
 insert into Beers (FratID,BeerName) Values (Null,'Natty Lite 10')
 insert into Beers (FratID,BeerName) Values (Null,'Natty Lite 11')
 insert into Beers (FratID,BeerName) Values (Null,'Natty Lite 12')
+
+--Select * from Fratboys
+
+--select * from Beers
+
+---List everybody who has a beer.
+---INNER JOIN
+--Select * from Fratboys
+--INNER JOIN BEERS
+--ON Fratboys.FratID = Beers.FratID
+
+---List everybody if they have a beer or not
+---But if they have a beer, list their beer
+---LEFT OUTER JOIN
+--Select * from Fratboys
+--LEFT OUTER JOIN Beers 
+--ON beers.FratID = Fratboys.FratID
+
+---List everybody who does not have a beer
+---LEFT OUTER JOIN with where clause
+--Select * from Fratboys
+--LEFT OUTER JOIN Beers 
+--ON beers.FratID = Fratboys.FratID
+--WHERE BeerID IS Null
+
+---List All beers not owned by anyone
+--Select * from Fratboys
+--RIGHT OUTER JOIN Beers 
+--ON beers.FratID = Fratboys.FratID
+--WHERE Beers.FratID IS Null
+
+---List all Fratboys and all Beers listing 
+---beer owners where appropriate
+--Select * from Fratboys
+-FULL JOIN Beers 
+--ON beers.FratID = Fratboys.FratID
